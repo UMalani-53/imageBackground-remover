6621fd57cfabee1f5b8705309651c8a501d95b21
udayagiri malani <umalanicm053@gmail.com>
2023-12-19 21:55:27 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var imgBox = document.querySelector(".img-box");
+    var imgWrap = document.querySelector(".img-wrap");
+    var originalImg = document.getElementById("originalImg");
+    var line = document.querySelector("#line");
+
+    originalImg.style.width = imgBox.offsetWidth + "px";
+
+    var leftSpace = imgBox.offsetLeft;
+
+    imgBox.onmousemove = function (e) {
+        var boxWidth = (e.pageX - leftSpace) + "px";
+        imgWrap.style.width = boxWidth;
+        line.style.left = boxWidth;
+    };
+});
